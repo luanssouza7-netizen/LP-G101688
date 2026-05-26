8ec6fd7718b1897a0280e1622ae68d76c78b8571
luanssouza7-netizen <luan.s.souza7@ba.estudante.senai.br>
2026-05-26 16:42:14 -0300

    Add files via upload

---
+++ ativasdade.js
@@ -0,0 +1,8 @@
+const notas = [10, 10, 10]
+
+console.log("\nExibindo nota \n")
+console.log(notas)
+
+console.log("\n Média Aritmética")
+const total = notas.reduce((soma, total) => soma + atual  /3,0)
+console.log(total)
---
+++ pagina-tabuada/script.js
@@ -0,0 +1,5 @@
+// FUnção que irá executar quando clicar no botão btnGerar.
+function gerarTabuada() {
+    // Pegar o valor do campo input HTML.
+    const numeroinput = document.getElementById('numeroinput')
+}
